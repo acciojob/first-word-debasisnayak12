bd83398ca95db2ea19bd2e15fa8a74e045e95631
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-19 10:25:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	if(s.length === 0 || !s){
+		return s;
+	}
+	let extractFirst = s.split(" ");
+	return extractFirst[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:"); 
 alert(firstWord(s));
